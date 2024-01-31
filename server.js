2b3cbaf5ab0921b07f1ca13fa43bee3165d02b35
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2024-01-31 14:04:49 +0530

    done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const http = require('http')
 const moment = require('moment');
 const socketio = require('socket.io');
-const PORT = process.env.PORT || 5003;
+const PORT = process.env.PORT || 5005;
 
 const app = express();
 const server = http.createServer(app);
