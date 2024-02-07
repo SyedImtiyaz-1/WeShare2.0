4f127dca39c05609b7406047fb278397021e916a
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2024-02-08 05:24:40 +0530

    Project Done

--- server.js
+++ server.js
@@ -1,11 +1,110 @@
+// const path = require("path");
+// const express = require("express");
+// const http = require("http");
+// const moment = require("moment");
+// const socketio = require("socket.io");
+// const sqlite3 = require("sqlite3").verbose();
+// const mysql = require("mysql");
+
+// const PORT = process.env.PORT || 5003;
+
+// const app = express();
+// const server = http.createServer(app);
+
+// const io = socketio(server);
+
+// // SQLite setup
+// const db = new sqlite3.Database("weshareData.db", (err) => {
+//   if (err) {
+//     console.error("Error connecting to SQLite database:", err.message);
+//   } else {
+//     console.log("Connected to SQLite database");
+//     // Create a messages table if it doesn't exist
+//     db.run("CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, roomid TEXT, username TEXT, content TEXT, timestamp TEXT)");
+//   }
+// });
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+// let rooms = {};
+// let socketroom = {};
+// let socketname = {};
+// let micSocket = {};
+// let videoSocket = {};
+// let roomBoard = {};
+
+// io.on('connect', socket => {
+//   socket.on("join room", (roomid, username) => {
+//     socket.join(roomid);
+//     socketroom[socket.id] = roomid;
+//     socketname[socket.id] = username;
+//     micSocket[socket.id] = 'on';
+//     videoSocket[socket.id] = 'on';
+
+//     if (rooms[roomid] && rooms[roomid].length > 0) {
+//       rooms[roomid].push(socket.id);
+//       socket.to(roomid).emit('message', `${username} joined the room.`, 'Bot', moment().format(
+//         "h:mm a"
+//       ));
+//       io.to(socket.id).emit('join room', rooms[roomid].filter(pid => pid != socket.id), socketname, micSocket, videoSocket);
+//     } else {
+//       rooms[roomid] = [socket.id];
+//       io.to(socket.id).emit('join room', null, null, null, null);
+//     }
+
+//     io.to(roomid).emit('user count', rooms[roomid].length);
+//   });
+
+//     socket.on("action", (msg) => {
+//       if (msg == "mute") micSocket[socket.id] = "off";
+//       else if (msg == "unmute") micSocket[socket.id] = "on";
+//       else if (msg == "videoon") videoSocket[socket.id] = "on";
+//       else if (msg == "videooff") videoSocket[socket.id] = "off";
+
+//       socket.to(socketroom[socket.id]).emit("action", msg, socket.id);
+//     });
+
+//     socket.on('message', (msg, username, roomid) => {
+//       // Emit the message to all clients in the room
+//       io.to(roomid).emit('message', msg, username, moment().format("h:mm a"));
+
+//       // Store the message in the database
+//       db.run("INSERT INTO messages (roomid, username, content, timestamp) VALUES (?, ?, ?, ?)",
+//         [roomid, username, msg, moment().format("YYYY-MM-DD HH:mm:ss")],
+//         (err) => {
+//           if (err) {
+//             console.error("Error inserting message into the SQLite database:", err);
+//           } else {
+//             console.log("Message inserted into the SQLite database");
+//           }
+//         });
+//     });
+
+//     socket.on("disconnect", () => {
+//       if (!socketroom[socket.id]) return;
+//       delete socketroom[socket.id];
+
+//       // SQLite: Delete user from the database
+//       db.close((err) => {
+//         if (err) {
+//           console.error("Error closing SQLite database:", err.message);
+//         } else {
+//           console.log("SQLite database connection closed");
+//         }
+//       });
+//     });
+//   });
+
+// server.listen(PORT, () =>
+//   console.log(`Server is up and running on port ${PORT}`)
+// );
+
 const path = require("path");
 const express = require("express");
 const http = require("http");
 const moment = require("moment");
 const socketio = require("socket.io");
 const sqlite3 = require("sqlite3").verbose();
-const mysql = require("mysql");
-
 const PORT = process.env.PORT || 5003;
 
 const app = express();
@@ -20,7 +119,9 @@ const db = new sqlite3.Database("weshareData.db", (err) => {
   } else {
     console.log("Connected to SQLite database");
     // Create a messages table if it doesn't exist
-    db.run("CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, roomid TEXT, username TEXT, content TEXT, timestamp TEXT)");
+    db.run(
+      "CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, roomid TEXT, username TEXT, content TEXT, timestamp TEXT)"
+    );
   }
 });
 
@@ -33,70 +134,131 @@ let micSocket = {};
 let videoSocket = {};
 let roomBoard = {};
 
-io.on('connect', socket => {
+io.on("connect", (socket) => {
   socket.on("join room", (roomid, username) => {
     socket.join(roomid);
     socketroom[socket.id] = roomid;
     socketname[socket.id] = username;
-    micSocket[socket.id] = 'on';
-    videoSocket[socket.id] = 'on';
+    micSocket[socket.id] = "on";
+    videoSocket[socket.id] = "on";
 
     if (rooms[roomid] && rooms[roomid].length > 0) {
       rooms[roomid].push(socket.id);
-      socket.to(roomid).emit('message', `${username} joined the room.`, 'Bot', moment().format(
-        "h:mm a"
-      ));
-      io.to(socket.id).emit('join room', rooms[roomid].filter(pid => pid != socket.id), socketname, micSocket, videoSocket);
+      socket
+        .to(roomid)
+        .emit(
+          "message",
+          `${username} joined the room.`,
+          "Bot",
+          moment().format("h:mm a")
+        );
+      io.to(socket.id).emit(
+        "join room",
+        rooms[roomid].filter((pid) => pid != socket.id),
+        socketname,
+        micSocket,
+        videoSocket
+      );
     } else {
       rooms[roomid] = [socket.id];
-      io.to(socket.id).emit('join room', null, null, null, null);
+      io.to(socket.id).emit("join room", null, null, null, null);
     }
 
-    io.to(roomid).emit('user count', rooms[roomid].length);
-  });  
+    io.to(roomid).emit("user count", rooms[roomid].length);
+  });
 
-    socket.on("action", (msg) => {
-      if (msg == "mute") micSocket[socket.id] = "off";
-      else if (msg == "unmute") micSocket[socket.id] = "on";
-      else if (msg == "videoon") videoSocket[socket.id] = "on";
-      else if (msg == "videooff") videoSocket[socket.id] = "off";
-      
-      socket.to(socketroom[socket.id]).emit("action", msg, socket.id);
-    });
-    
+  socket.on("action", (msg) => {
+    if (msg == "mute") micSocket[socket.id] = "off";
+    else if (msg == "unmute") micSocket[socket.id] = "on";
+    else if (msg == "videoon") videoSocket[socket.id] = "on";
+    else if (msg == "videooff") videoSocket[socket.id] = "off";
 
-    socket.on('message', (msg, username, roomid) => {
-      // Emit the message to all clients in the room
-      io.to(roomid).emit('message', msg, username, moment().format("h:mm a"));
-  
-      // Store the message in the database
-      db.run("INSERT INTO messages (roomid, username, content, timestamp) VALUES (?, ?, ?, ?)",
-        [roomid, username, msg, moment().format("YYYY-MM-DD HH:mm:ss")],
-        (err) => {
-          if (err) {
-            console.error("Error inserting message into the SQLite database:", err);
-          } else {
-            console.log("Message inserted into the SQLite database");
-          }
-        });
-    });
-    
-    
-    socket.on("disconnect", () => {
-      if (!socketroom[socket.id]) return;
-      delete socketroom[socket.id];
-      
-      // SQLite: Delete user from the database
-      db.close((err) => {
+    socket.to(socketroom[socket.id]).emit("action", msg, socket.id);
+  });
+
+  socket.on("video-offer", (offer, sid) => {
+    socket
+      .to(sid)
+      .emit(
+        "video-offer",
+        offer,
+        socket.id,
+        socketname[socket.id],
+        micSocket[socket.id],
+        videoSocket[socket.id]
+      );
+  });
+
+  socket.on("video-answer", (answer, sid) => {
+    socket.to(sid).emit("video-answer", answer, socket.id);
+  });
+
+  socket.on("new icecandidate", (candidate, sid) => {
+    socket.to(sid).emit("new icecandidate", candidate, socket.id);
+  });
+
+  socket.on("message", (msg, username, roomid) => {
+    io.to(roomid).emit("message", msg, username, moment().format("h:mm a"));
+
+    // Store the message in the database
+    db.run(
+      "INSERT INTO messages (roomid, username, content, timestamp) VALUES (?, ?, ?, ?)",
+      [roomid, username, msg, moment().format("YYYY-MM-DD HH:mm:ss")],
+      (err) => {
         if (err) {
-          console.error("Error closing SQLite database:", err.message);
+          console.error(
+            "Error inserting message into the SQLite database:",
+            err
+          );
         } else {
-          console.log("SQLite database connection closed");
+          console.log("Message inserted into the SQLite database");
         }
-      });
+      }
+    );
+  });
+
+  socket.on("getCanvas", () => {
+    if (roomBoard[socketroom[socket.id]])
+      socket.emit("getCanvas", roomBoard[socketroom[socket.id]]);
+  });
+
+  socket.on("draw", (newx, newy, prevx, prevy, color, size) => {
+    socket
+      .to(socketroom[socket.id])
+      .emit("draw", newx, newy, prevx, prevy, color, size);
+  });
+
+  socket.on("clearBoard", () => {
+    socket.to(socketroom[socket.id]).emit("clearBoard");
+  });
+
+  socket.on("store canvas", (url) => {
+    roomBoard[socketroom[socket.id]] = url;
+  });
+
+  socket.on("disconnect", () => {
+    if (!socketroom[socket.id]) return;
+        socket.to(socketroom[socket.id]).emit('message', `${socketname[socket.id]} left the chat.`, `Bot`, moment().format(
+            "h:mm a"
+        ));
+        socket.to(socketroom[socket.id]).emit('remove peer', socket.id);
+        var index = rooms[socketroom[socket.id]].indexOf(socket.id);
+        rooms[socketroom[socket.id]].splice(index, 1);
+        io.to(socketroom[socket.id]).emit('user count', rooms[socketroom[socket.id]].length);
+        delete socketroom[socket.id];
+        console.log('--------------------');
+        console.log(rooms[socketroom[socket.id]]);
+
+    // Close the SQLite database connection
+    db.close((err) => {
+      if (err) {
+        console.error("Error closing SQLite database:", err.message);
+      } else {
+        console.log("SQLite database connection closed");
+      }
     });
   });
-  
+});
 
 server.listen(PORT, () =>
   console.log(`Server is up and running on port ${PORT}`)
